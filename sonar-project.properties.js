21259b3e6a1ab9fe47f04b51d45b503bd1299f8a
Avinash_Kondaveti <kondavetiavinash3@gmail.com>
2023-07-17 14:08:27 +0530

    new jenkinsfile

---
+++ sonar-project.properties.js
@@ -0,0 +1,20 @@
+
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://172.18.102.114:9000',
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey': 'nodejs',
+	    'sonar.login': 'ssqa_66697c98cba617b4bc7c19ffe654db0b93df0702',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin123',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+} ); 
